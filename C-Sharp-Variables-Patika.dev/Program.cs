54c5ec72eb0817ce81bcefee00a03fc57d46ae08
bacmk <ozancakmak867@gmail.com>
2022-08-25 10:12:52 +0300

    Proje dosyası ekle.

---
+++ C-Sharp-Variables-Patika.dev/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace C_Sharp_Variables_Patika.dev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           # De-i-kenler
+            // Değişken Tanımlama
+            int deger;
+            string degisken = null;// Değişkenler büyük küçük harf duyarlıdır.
+
+            string Degişken = null;
+            string 3degisken = "";// Değişkenin başında sayı bulunamaz.
+
+            string system = "";//Özell isimler verilemez.
+            string degisken+veri = "";//İşlem operatörü içeremez.
+            string degisken_veri = "";// Alt tire kullanabilirsin.
+
+
+
+            Console.WriteLine(deger);
+            Değişken Tipleri
+            string degisken = "";
+            byte b = 5; // 1 byte
+            sbyte c = 5; // 1 byte
+            short s = 5; // 2 byte
+            ushort u = 5; // 2 byte
+            Int16 i16 = 2; // 2 byte
+            int i = 2; // 4  byte
+            Int32 i32 = 2;// 4 byte
+            Int64 i64 = 2; // 8 byte
+            uint u32 = 2;// 4 byte
+            long l = 4;// 8 byte
+            ulong r = 4;// 8 byte
+            float f = 5; // 4 byte// reel sayılar için
+            double d = 5;//8 byte
+            decimal de = 5; // 16 byte
+
+            char c = 'R'; // 2 byte
+            string str = "Rabia"; // sınırsız
+            bool b1 = true;
+
+            DateTime dt = "DateTime.Now";// Trih Ve Saat
+            object ol = "x";
+            object ol1 = 4;
+            object ol2 = 4.3;
+
+
+        }
+    }
+}
